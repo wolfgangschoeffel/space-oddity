5e7e118b718f3bc6e7d6ed980de7febad19bb026
Emanuel Tannert <post@etannert.de>
2017-01-10 14:42:06 +0100

    format main.js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 var vpW, vpH;
-var noop = function(){};
+var noop = function () {};
 var dResize = _.debounce(resize, 500);
 
 var mapwrap = document.getElementById('map-wrapper');
 
 var display = {
-    lat: document.getElementById('lat'),
-    lng: document.getElementById('lng')
+  lat: document.getElementById('lat'),
+  lng: document.getElementById('lng')
 };
 
-function resize(){
-    vpW = window.innerWidth;
-    vpH = window.innerHeight;
+function resize () {
+  vpW = window.innerWidth;
+  vpH = window.innerHeight;
 }
 
 window.addEventListener('resize', dResize);
 
-window.addEventListener('load', function(){
-    resize();
+window.addEventListener('load', function () {
+  resize();
 });
 
 
 
 var map;
-function initMap() {
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
-        disableDefaultUI: true
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+function initMap () {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    disableDefaultUI: true
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    google.maps.event.addListenerOnce(map, 'idle', function(){
-        getCoordinates();
-        classie.remove( document.body, 'loading' );
-    });
+  google.maps.event.addListenerOnce(map, 'idle', function () {
+    getCoordinates();
+    classie.remove(document.body, 'loading');
+  });
 }
 
 googleMapsApiLoader.load(initMap)
@@ -43,27 +43,26 @@ googleMapsApiLoader.load(initMap)
 
 
 
-function requestJSONP(url) {
-    try {
-        var script = document.createElement('script');
-        script.src = url;
+function requestJSONP (url) {
+  try {
+    var script = document.createElement('script');
+    script.src = url;
 
-        script.onload = function () {
-            this.remove();
-        };
+    script.onload = function () {
+        this.remove();
+    };
 
-        var head = document.getElementsByTagName('head')[0];
-        head.insertBefore(script, null);
-    }catch(e){
+    var head = document.getElementsByTagName('head')[0];
+    head.insertBefore(script, null);
+  } catch (e) {
 
-    }
-
+  }
 }
 
 
-function getCoordinates(){
-    var url = 'http://api.open-notify.org/iss-now.json?callback=iss.move';
-    requestJSONP(url);
+function getCoordinates () {
+  var url = 'http://api.open-notify.org/iss-now.json?callback=iss.move';
+  requestJSONP(url);
 }
 
 // Gegeben:
@@ -79,90 +78,90 @@ function getCoordinates(){
 
 
 var iss = {
-    lat: 0,
-    lng: 0,
-    lat0: undefined,
-    lng0: undefined,
-    queryInterval: 10000,
+  lat: 0,
+  lng: 0,
+  lat0: undefined,
+  lng0: undefined,
+  queryInterval: 10000,
 
-    move: function(data){
-        console.log('move iss');
-        iss.lat = data.iss_position.latitude;
-        iss.lng = data.iss_position.longitude;
+  move: function (data) {
+    console.log('move iss');
+    iss.lat = data.iss_position.latitude;
+    iss.lng = data.iss_position.longitude;
 
-        var now = Date.now();
-        var deltaT;
-        var deltaLat;
-        var deltaLng;
+    var now = Date.now();
+    var deltaT;
+    var deltaLat;
+    var deltaLng;
 
-        if( iss.lastTime ){
-            deltaT = now - iss.lastTime;
-            deltaLat = iss.lat - iss.lat0;
-            deltaLng = iss.lng - iss.lng0;
+    if (iss.lastTime) {
+      deltaT = now - iss.lastTime;
+      deltaLat = iss.lat - iss.lat0;
+      deltaLng = iss.lng - iss.lng0;
 
-            iss.vLat = deltaLat / deltaT;
-            iss.vLng = deltaLng / deltaT;
+      iss.vLat = deltaLat / deltaT;
+      iss.vLng = deltaLng / deltaT;
 
-            follower.start();
+      follower.start();
 
-        }else {
-            map.panTo(new google.maps.LatLng(iss.lat, iss.lng) );
-            classie.add(mapwrap, 'active');
-        }
-
-        iss.lat0 = iss.lat;
-        iss.lng0 = iss.lng;
-
-        if( iss.lastTime ){
-            window.setTimeout(getCoordinates, this.queryInterval);
-        }else {
-            window.setTimeout(getCoordinates, 500);
-        }
-
-        iss.lastTime = now;
+    } else {
+      map.panTo(new google.maps.LatLng(iss.lat, iss.lng));
+      classie.add(mapwrap, 'active');
     }
+
+    iss.lat0 = iss.lat;
+    iss.lng0 = iss.lng;
+
+    if (iss.lastTime) {
+      window.setTimeout(getCoordinates, this.queryInterval);
+    } else {
+      window.setTimeout(getCoordinates, 500);
+    }
+
+    iss.lastTime = now;
+  }
 };
 
 
 var follower = {
-    lat: 0,
-    lng: 0,
-    lat0: undefined,
-    lng0: undefined,
-    isActive: false,
-    updateInterval: 500,
+  lat: 0,
+  lng: 0,
+  lat0: undefined,
+  lng0: undefined,
+  isActive: false,
+  updateInterval: 500,
 
-    start: function(){
-        if( ! this.isActive ){
-            console.log( 'start follower' );
-            this.isActive = true;
-            this.update();
-        }
-    },
-
-    update: function(){
-        console.log('move follower');
-        var now = Date.now();
-
-        if( follower.lastTime ){
-            var deltaT = now - follower.lastTime;
-            follower.lat = follower.lat0 + iss.vLat * deltaT;
-            follower.lng = follower.lng0 + iss.vLng * deltaT;
-        }else {
-            follower.lat = iss.lat;
-            follower.lng = iss.lng;
-        }
-
-        follower.lat0 = follower.lat;
-        follower.lng0 = follower.lng;
-        follower.lastTime = now;
-
-        map.panTo(new google.maps.LatLng(follower.lat, follower.lng) );
-
-        display.lat.innerHTML = 'Lat:&nbsp;' + parseFloat(this.lat).toFixed(6);
-        display.lng.innerHTML = 'Long:&nbsp;' + parseFloat(this.lng).toFixed(6);
-
-        window.setTimeout(this.update.bind(this), this.updateInterval);
+  start: function () {
+    if (!this.isActive) {
+      console.log('start follower');
+      this.isActive = true;
+      this.update();
     }
+  },
+
+  update: function () {
+    console.log('move follower');
+    var now = Date.now();
+
+    if (follower.lastTime) {
+      var deltaT = now - follower.lastTime;
+      follower.lat = follower.lat0 + iss.vLat * deltaT;
+      follower.lng = follower.lng0 + iss.vLng * deltaT;
+    } else {
+      follower.lat = iss.lat;
+      follower.lng = iss.lng;
+    }
+
+    follower.lat0 = follower.lat;
+    follower.lng0 = follower.lng;
+    follower.lastTime = now;
+
+    map.panTo(new google.maps.LatLng(follower.lat, follower.lng));
+
+    display.lat.innerHTML = 'Lat:&nbsp;' + parseFloat(this.lat).toFixed(6);
+    display.lng.innerHTML = 'Long:&nbsp;' + parseFloat(this.lng).toFixed(6);
+
+    window.setTimeout(this.update.bind(this), this.updateInterval);
+  }
 };
 
