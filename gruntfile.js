6c7e5791af7c6d09c0a2f7662fd46b35189a7b5d
Emanuel Tannert <post@etannert.de>
2017-01-10 15:29:35 +0100

    remove modernizr since it's not used anyway

--- gruntfile.js
+++ gruntfile.js
@@ -68,55 +68,6 @@ module.exports = function(grunt) {
             dist: [
                 'css/main.min.css'
             ]
-        },
-
-        modernizr: {
-
-            dist: {
-                // [REQUIRED] Path to the build you're using for development.
-                'devFile' : 'js/modernizr-latest.js',
-                // [REQUIRED] Path to save out the built file.
-                'outputFile' : 'js/modernizr-custom.js',
-
-                // Based on default settings on http://modernizr.com/download/
-                'extra' : {
-                    'shiv' : true,
-                    'printshiv' : false,
-                    'load' : false,
-                    'mq' : false,
-                    'cssclasses' : true
-                },
-
-                // Based on default settings on http://modernizr.com/download/
-                'extensibility' : {
-                    'addtest' : false,
-                    'prefixed' : false,
-                    'teststyles' : false,
-                    'testprops' : false,
-                    'testallprops' : false,
-                    'hasevents' : false,
-                    'prefixes' : false,
-                    'domprefixes' : false
-                },
-
-                // By default, source is uglified before saving
-                'uglify' : false,
-
-                // Define any tests you want to implicitly include.
-                'tests' : [ 'touch'],
-
-                // By default, this task will crawl your project for references to Modernizr tests.
-                // Set to false to disable.
-                'parseFiles' : false,
-
-                // When parseFiles = true, matchCommunityTests = true will attempt to
-                // match user-contributed tests.
-                'matchCommunityTests' : true,
-
-                // Have custom Modernizr tests? Add paths to their location here.
-                'customTests' : []
-            }
-
         }
 
     });
@@ -126,5 +77,4 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-modernizr');
 };
